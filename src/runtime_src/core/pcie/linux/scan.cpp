9d36b28b9f2d17c52613e97ed5213f1eb7a93c89
CW Lan <chienwei@xilinx.com>
2019-12-10 15:08:00 -0800

    misc

--- src/runtime_src/core/pcie/linux/scan.cpp
+++ src/runtime_src/core/pcie/linux/scan.cpp
@@ -60,7 +60,7 @@ static std::string get_name(const std::string& dir, const std::string& subdir)
 // Helper to find subdevice directory name
 // Assumption: all subdevice's sysfs directory name starts with subdevice name!!
 static int get_subdev_dir_name(const std::string& dir,
-    const std::string& subDevName, uint idx, std::string& subdir)
+    const std::string& subDevName, std::string& subdir)
 {
     DIR *dp;
     size_t sub_nm_sz = subDevName.size();
@@ -97,7 +97,7 @@ std::string pcidev::pci_device::get_sysfs_path(const std::string& subdev,
     const std::string& entry)
 {
     std::string subdir;
-    if (get_subdev_dir_name(sysfs_root + sysfs_name, subdev, 0, subdir) != 0)
+    if (get_subdev_dir_name(sysfs_root + sysfs_name, subdev, subdir) != 0)
         return "";
 
     std::string path = sysfs_root;
