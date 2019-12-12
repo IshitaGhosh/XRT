339a4417c3b00a183a4b41e15e1f47075348537c
CW Lan <chienwei@xilinx.com>
2019-12-12 13:06:36 -0800

    misc

--- src/runtime_src/xrt/device/hal.h
+++ src/runtime_src/xrt/device/hal.h
@@ -662,6 +662,12 @@ public:
   }
 
   virtual operations_result<std::string>
+  getSubdevPath(const std::string& subdev, uint idx)
+  {
+    return operations_result<std::string>();
+  }
+
+  virtual operations_result<std::string>
   getDebugIPlayoutPath()
   {
     return operations_result<std::string>();
