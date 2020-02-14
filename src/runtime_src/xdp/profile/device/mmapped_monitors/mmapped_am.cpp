f9a376e76155faf1a539fa7e2860df35b9d4a6d5
IshitaGhosh <ighosh.account@gmail.com>
2020-02-14 14:04:38 -0800

    Add new specializations for all Profile IP monitors to support new open+mmap on device drivers for the monitors. Currently this flow will be supported for PCIe Linux devices only

---
+++ src/runtime_src/xdp/profile/device/mmapped_monitors/mmapped_am.cpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2020 Xilinx Inc - All rights reserved
+ * Xilinx Debug & Profile (XDP) APIs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may
+ * not use this file except in compliance with the License. A copy of the
+ * License is located at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <sys/fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "mmapped_am.h"
+
+namespace xdp {
+
+MMappedAM::MMappedAM(Device* handle, uint64_t index, uint64_t instIdx, debug_ip_data* data)
+         : AM(handle, index, data),
+           instance_index(instIdx)
+{
+  // Open AM Device Driver File
+  std::string subDev("accel_mon");
+  std::string driverFileName = getDevice()->getSubDevicePath(subDev, instance_index);
+
+  driver_FD = open(driverFileName.c_str(), O_RDWR);
+  if(-1 == driver_FD) {
+    // error
+  }
+
+  // mmap opened device driver file /* for now just 1 page size */
+  int pgSz = getpagesize();
+  mapped_device = (char*)mmap(NULL, pgSz, PROT_READ | PROT_WRITE, MAP_SHARED, driver_FD, 0);
+  if(mapped_device == MAP_FAILED) {
+    std::cout << " ERROR : Failed to mmap AM " << std::endl;
+  }
+}
+
+MMappedAM::~MMappedAM()
+{
+  munmap(mapped_device, getpagesize() /* for now */);
+  close(driver_FD);
+}
+
+int MMappedAM::read(uint64_t offset, size_t size, void* data)
+{
+  memcpy((char*)data, mapped_device + offset, size);
+  return size;
+}
+
+int MMappedAM::write(uint64_t offset, size_t size, void* data)
+{
+  memcpy(mapped_device + offset, (char*)data, size);
+  return size;
+}
+
+}
+
---
+++ src/runtime_src/xdp/profile/device/mmapped_monitors/mmapped_traceFifoFull.cpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2020 Xilinx Inc - All rights reserved
+ * Xilinx Debug & Profile (XDP) APIs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may
+ * not use this file except in compliance with the License. A copy of the
+ * License is located at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <sys/fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "mmapped_traceFifoFull.h"
+
+namespace xdp {
+
+MMappedTraceFifoFull::MMappedTraceFifoFull(Device* handle, uint64_t index, uint64_t instIdx, debug_ip_data* data)
+                    : TraceFifoFull(handle, index, data),
+                      instance_index(instIdx)
+{
+  // Open TraceFifoFull Device Driver File
+  std::string subDev("trace_fifo_full");
+  std::string driverFileName = getDevice()->getSubDevicePath(subDev, instance_index);
+
+  driver_FD = open(driverFileName.c_str(), O_RDWR);
+  if(-1 == driver_FD) {
+    // error
+  }
+
+  // mmap opened device driver file /* for now just 1 page size */
+  int pgSz = getpagesize();
+  mapped_device = (char*)mmap(NULL, pgSz, PROT_READ | PROT_WRITE, MAP_SHARED, driver_FD, 0);
+  if(mapped_device == MAP_FAILED) {
+    std::cout << " ERROR : Failed to mmap TraceFifoFull " << std::endl;
+  }
+}
+
+MMappedTraceFifoFull::~MMappedTraceFifoFull()
+{
+  munmap(mapped_device, getpagesize() /* for now */);
+  close(driver_FD);
+}
+
+int MMappedTraceFifoFull::read(uint64_t offset, size_t size, void* data)
+{
+  memcpy((char*)data, mapped_device + offset, size);
+  return size;
+}
+
+int MMappedTraceFifoFull::write(uint64_t offset, size_t size, void* data)
+{
+  memcpy(mapped_device + offset, (char*)data, size);
+  return size;
+}
+
+}
+
