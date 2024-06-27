e68d93f6e2cb99b822cdc53757b9d0bab7d1fc73
Ishita Ghosh <ishita.ghosh@amd.com>
2024-06-27 14:27:53 -0700

    Add callbacks for invoking AIE Halt Plugin
    Signed-off-by: Ishita Ghosh <ishita.ghosh@amd.com>

--- src/runtime_src/core/common/xdp/profile.cpp
+++ src/runtime_src/core/common/xdp/profile.cpp
@@ -131,7 +131,6 @@ end_debug(void* handle)
 
 } // end namespace xrt_core::xdp::aie::debug
 
-
 namespace xrt_core::xdp::ml_timeline {
 
 std::function<void (void*)> update_device_cb;
@@ -180,6 +179,55 @@ finish_flush_device(void* handle)
 }
 
 } // end namespace xrt_core::xdp::ml_timeline
+
+namespace xrt_core::xdp::aie_halt {
+
+std::function<void (void*)> update_device_cb;
+std::function<void (void*)> finish_flush_device_cb;
+
+void
+register_callbacks(void* handle)
+{ 
+  #ifdef XDP_CLIENT_BUILD
+    using ftype = void (*)(void*);
+
+    update_device_cb = reinterpret_cast<ftype>(xrt_core::dlsym(handle, "updateDeviceAieHalt"));
+    finish_flush_device_cb = reinterpret_cast<ftype>(xrt_core::dlsym(handle, "finishflushDeviceAieHalt"));
+  #else
+    (void)handle;
+  #endif
+
+}
+
+void
+warning_callbacks()
+{
+}
+
+void
+load()
+{
+  static xrt_core::module_loader xdp_loader("xdp_aie_halt_plugin",
+                                                register_callbacks,
+                                                warning_callbacks);
+}
+
+// Make connections
+void
+update_device(void* handle)
+{
+  if (update_device_cb)
+    update_device_cb(handle);
+}
+
+void
+finish_flush_device(void* handle)
+{
+  if (finish_flush_device_cb)
+    finish_flush_device_cb(handle);
+}
+
+} // end namespace xrt_core::xdp::aie_halt
 
 namespace xrt_core::xdp::pl_deadlock {
 
@@ -293,7 +341,8 @@ update_device(void* handle)
   if (xrt_core::config::get_ml_timeline()
       || xrt_core::config::get_aie_profile()
       || xrt_core::config::get_aie_trace()
-      || xrt_core::config::get_aie_debug()) {
+      || xrt_core::config::get_aie_debug()
+      || xrt_core::config::get_aie_halt()) {
     /* All the above plugins are dependent on xdp_core library. So,
      * explicitly load it to avoid library search issue in implicit loading.
      */
@@ -345,6 +394,16 @@ update_device(void* handle)
     xrt_core::xdp::ml_timeline::update_device(handle);
   }
 
+  if (xrt_core::config::get_aie_halt()) {
+    try {
+      xrt_core::xdp::aie_halt::load();
+    }
+    catch (...) {
+      return;
+    }
+    xrt_core::xdp::aie_halt::update_device(handle);
+  }
+
 #else
 
   if (xrt_core::config::get_pl_deadlock_detection() 
@@ -373,7 +432,8 @@ finish_flush_device(void* handle)
     xrt_core::xdp::aie::debug::end_debug(handle);
   if (xrt_core::config::get_ml_timeline())
     xrt_core::xdp::ml_timeline::finish_flush_device(handle);
-
+  if (xrt_core::config::get_aie_halt())
+    xrt_core::xdp::aie_halt::finish_flush_device(handle);
 #else
 
   if (xrt_core::config::get_pl_deadlock_detection()
